e1f9fd8a38c618cb2b50070043fc2b75e4d4ca4b
Yassine El Bouziady <yassine.elbouziady@flowdigitalstudio.com>
2021-07-03 12:37:36 +0100

    Feat: Setup a clean Next.js project with TypeScript, ESLint and Prettier (#1442294138)

---
+++ src/components/Test.tsx
@@ -0,0 +1,12 @@
+function Test(): JSX.Element {
+    return (
+        <div>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Soluta
+            iusto pariatur totam quo saepe perspiciatis ullam non, est atque
+            repudiandae dolorem error commodi sit fugiat aperiam nobis voluptate
+            placeat fuga?
+        </div>
+    );
+}
+
+export default Test;
