29291948a84dafe01a24044afe38e4428b1c9c69
Yassine El Bouziady <yassine.elbouziady@flowdigitalstudio.com>
2021-07-11 16:09:02 +0100

    Fix: Corrine image format (#1442657297)

--- src/components/HomePage/MotDeCorrine/index.tsx
+++ src/components/HomePage/MotDeCorrine/index.tsx
@@ -1,15 +1,15 @@
 import { Flex, Box, HStack, Text, Button, Link } from '@chakra-ui/react';
 import NextLink from 'next/link';
-import Face from '@/assets/HomePage/Corrine/Face.svg';
 import Quote from '@/assets/HomePage/Corrine/quote.svg';
 import ArrowRight from '@/assets/HomePage/PopularProducts/arrowRight.svg';
+import Image from 'next/image';
 
 function Corrine(): JSX.Element {
     return (
         <Flex as="section" px="20px" mt="65px">
             <HStack maxW="1200px" mx="auto" w="100%" justifyContent="space-between" alignItems="center">
                 <Box mb="28.57px">
-                    <Face />
+                    <Image src="/assets/images/Corrine/face.png" width={437} height={468} quality={100} alt="corrine cloix face" />
                 </Box>
                 <Box>
                     <Quote />
