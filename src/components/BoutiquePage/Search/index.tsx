7d552e736c9fe739a2a6053d84db8a35f72e6ad2
Yassine El Bouziady <yassine.elbouziady@flowdigitalstudio.com>
2021-07-13 22:13:36 +0100

    Fix: style of search button on desktop (#1459695475)

--- src/components/BoutiquePage/Search/index.tsx
+++ src/components/BoutiquePage/Search/index.tsx
@@ -1,9 +1,8 @@
-import { Box, Button, HStack, Input, IconButton, useMediaQuery } from '@chakra-ui/react';
+import { Box, Button, HStack, Input, IconButton } from '@chakra-ui/react';
 import SearchIcon from '@/assets/BoutiquePage/Search/search.svg';
 import { FormEvent, useState } from 'react';
 
 function Search(): JSX.Element {
-    const [isLargerThan40em] = useMediaQuery('(min-width:40em)');
     const [search, setSearch] = useState('');
     const handleInputSearch = (e: FormEvent<HTMLInputElement>): void => {
         setSearch(e.currentTarget.value);
@@ -12,6 +11,7 @@ function Search(): JSX.Element {
         e.preventDefault();
         console.log(search);
     };
+
     return (
         <Box max="1200px" px="20px" mx="auto" w="100%" mt="-37px" pos="relative" zIndex="1">
             <HStack
@@ -42,46 +42,47 @@ function Search(): JSX.Element {
                     onChange={handleInputSearch}
                     flex="1"
                 />
-                {isLargerThan40em ? (
-                    <Button
-                        fontFamily="montserrat"
-                        fontWeight="700"
-                        fontSize="14px"
-                        lineHeight="56px"
-                        letterSpacing="0.2px"
-                        leftIcon={<SearchIcon />}
-                        iconSpacing="17px"
-                        color="white"
-                        flex="0 0 224px"
-                        h="100%"
-                        borderRadius="9px"
-                        bg="#811D65"
-                        _hover={{ bg: '#811D65' }}
-                        _active={{ bg: '#811D65' }}
-                        borderTopLeftRadius="0px"
-                        borderBottomLeftRadius="0px"
-                        borderTopRightRadius="9px"
-                        borderBottomRightRadius="9px"
-                        type="submit"
-                    >
-                        Rechercher
-                    </Button>
-                ) : (
-                    <IconButton
-                        aria-label="chercher des produits"
-                        icon={<SearchIcon />}
-                        borderTopLeftRadius="0px"
-                        borderBottomLeftRadius="0px"
-                        borderTopRightRadius="9px"
-                        borderBottomRightRadius="9px"
-                        type="submit"
-                        bg="#811D65"
-                        _hover={{ bg: '#811D65' }}
-                        _active={{ bg: '#811D65' }}
-                        flex="0 0 57px"
-                        h="100%"
-                    />
-                )}
+
+                <Button
+                    fontFamily="montserrat"
+                    fontWeight="700"
+                    fontSize="14px"
+                    lineHeight="56px"
+                    letterSpacing="0.2px"
+                    leftIcon={<SearchIcon />}
+                    iconSpacing="17px"
+                    color="white"
+                    flex="0 0 224px"
+                    h="100%"
+                    borderRadius="9px"
+                    bg="#811D65"
+                    _hover={{ bg: '#811D65' }}
+                    _active={{ bg: '#811D65' }}
+                    borderTopLeftRadius="0px"
+                    borderBottomLeftRadius="0px"
+                    borderTopRightRadius="9px"
+                    borderBottomRightRadius="9px"
+                    type="submit"
+                    display={['none', null, 'inline-flex']}
+                >
+                    Rechercher
+                </Button>
+
+                <IconButton
+                    aria-label="chercher des produits"
+                    icon={<SearchIcon />}
+                    borderTopLeftRadius="0px"
+                    borderBottomLeftRadius="0px"
+                    borderTopRightRadius="9px"
+                    borderBottomRightRadius="9px"
+                    type="submit"
+                    bg="#811D65"
+                    _hover={{ bg: '#811D65' }}
+                    _active={{ bg: '#811D65' }}
+                    flex="0 0 57px"
+                    h="100%"
+                    display={['inline-flex', null, 'none']}
+                />
             </HStack>
         </Box>
     );
