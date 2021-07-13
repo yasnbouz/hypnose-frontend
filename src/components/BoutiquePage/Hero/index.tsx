c970df14555f38a8076f8dfbfa36f50bd726164e
Yassine El Bouziady <yassine.elbouziady@flowdigitalstudio.com>
2021-07-13 09:43:36 +0100

    Add: responsive to slider page boutique (#1459694909)

--- src/components/BoutiquePage/Hero/index.tsx
+++ src/components/BoutiquePage/Hero/index.tsx
@@ -1,4 +1,4 @@
-import { Box, Flex, Text, HStack } from '@chakra-ui/react';
+import { Box, Flex, Text, SimpleGrid } from '@chakra-ui/react';
 import Anime from '@/assets/BoutiquePage/Hero/Frame.svg';
 import FrameLeft from '@/assets/HomePage/hero/FrameLeft.svg';
 import FrameRight from '@/assets/HomePage/hero/FrameRight.svg';
@@ -7,24 +7,34 @@ import FrameTop from '@/assets/HomePage/hero/FrameTop.svg';
 function Hero(): JSX.Element {
     return (
         <Flex as="section" h="447px" bg="#F7F1F6" pos="relative" px="20px" overflow="hidden">
-            <HStack maxWidth="1200px" w="100%" margin="auto" justifyContent="space-between" alignItems="center">
+            <SimpleGrid columns={[1, 1, 2]} justifyItems="center" alignItems="center" maxWidth="1200px" spacingY="27px" w="100%" margin="auto">
                 <Box>
-                    <Text as="h1" fontFamily="montserrat" fontWeight="500" fontSize="32px" lineHeight="56px" letterSpacing="0.2px" color="#373F41" maxW="632px">
+                    <Text
+                        as="h1"
+                        fontFamily="montserrat"
+                        fontWeight={['400', null, null, '500']}
+                        fontSize={['24px', null, null, '32px']}
+                        lineHeight={['29.26px', null, null, '40px']}
+                        letterSpacing="0.2px"
+                        color="#373F41"
+                        maxW={['270px', null, null, '632px']}
+                        textAlign={['center', null, 'left']}
+                    >
                         Profitez du pouvoir de l&apos;inconscient pour atteindre vos objectifs
                     </Text>
                 </Box>
-                <Box>
+                <Box w={['295px', null, null, '381px']} h={['227px', null, null, '292px']}>
                     <Anime />
                 </Box>
-            </HStack>
+            </SimpleGrid>
 
-            <Box pos="absolute" w="275px" h="375px" left="-5px" bottom="-45px" opacity="0.4" aria-hidden="true">
+            <Box display={['none', null, null, 'block']} pos="absolute" w="275px" h="375px" left="-5px" bottom="-45px" opacity="0.4" aria-hidden="true">
                 <FrameLeft />
             </Box>
-            <Box pos="absolute" w="256px" h="587px" right="0" top="0" opacity="0.4" aria-hidden="true">
+            <Box display={['none', null, null, 'block']} pos="absolute" w="256px" h="587px" right="0" top="0" opacity="0.4" aria-hidden="true">
                 <FrameRight />
             </Box>
-            <Box pos="absolute" w="407px" h="316px" left="50%" transform="translateX(-50%)" top="-27px" opacity="0.4" aria-hidden="true">
+            <Box display={['none', null, null, 'block']} pos="absolute" w="407px" h="316px" left="50%" transform="translateX(-50%)" top="-27px" opacity="0.4" aria-hidden="true">
                 <FrameTop />
             </Box>
         </Flex>
